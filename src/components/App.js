03d605d691a2444d7e6a2ae0db8c561b8ac29ef4
kuldeep singh <pssingh1145@gmail.com>
2023-03-05 19:09:36 +0530

    Create Home Page and Display Posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { getPosts } from "../api";
+import { Home } from '../pages';
+import { Loader } from './';
 
 function App() {
+  const [posts,setPosts]=useState([]);
+  const [loading,setLoading]=useState(true);
+
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+      const response=await getPosts();
+      //console.log('response',response);
+
+      if(response.success){
+        setPosts(response.data.posts);
+      }
+      setLoading(false);
+    }
+    fetchPosts();
+  },[]);
+
+  if(loading){
+    return <Loader />;
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home posts={posts}/>
     </div>
   );
 }
